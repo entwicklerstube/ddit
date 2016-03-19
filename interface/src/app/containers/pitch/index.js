29a1520f1b86479c4e446056e915d5bda7395a28
michaelzoidl <michael@zoidl.de>
2016-03-19 18:06:39 +0100

    update interface

--- interface/src/app/containers/pitch/index.js
+++ interface/src/app/containers/pitch/index.js
@@ -1,4 +1,6 @@
-import React, {Component}                   from 'react';
+import React, {Component} from 'react';
+
+import Overview from '../../components/overview';
 
 export default class extends Component {
   constructor(props) {
@@ -7,9 +9,22 @@ export default class extends Component {
 
   render() {
     return (
-      <div className="pitch">
-        <div className='tv-mock'>aef</div>
+      <div className='pitch'>
+        <div className='tv-mock'>
+          <div className='tv-screen'>
+            <div className='tv-wrapper'>
+              <Overview/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
+
+
+// Video code
+// <video src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" autoPlay>
+//   <source  type="video/mp4" />
+//   Your browser doesn't support HTML5 video tag.
+// </video>
