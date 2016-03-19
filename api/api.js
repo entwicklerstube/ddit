2f5f6e885cdbdf00ebb4663efbe5a03b8651d49c
Markus Springer <markus.springer1994@googlemail.com>
2016-03-19 18:04:34 +0100

    Update API

--- api/api.js
+++ api/api.js
@@ -2,6 +2,20 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var r = require('rethinkdb');
+var http = require('http');
+
+
+// routes will go here
+
+// start the server
+console.log('Server started!');
+
+var bodyParser = require('body-parser');
+
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
 
 var connection = null;
 r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
@@ -9,15 +23,53 @@ r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
     connection = conn;
 });
 
+
 server.listen(3000);
 
 app.get('/', function (req, res) {
-    res.send("asdfasdf");
+    res.sendfile(__dirname + '/index.html');
 });
 
+
+/* START SPORTSRADAR */
+
+app.post('/inputRoteKarten', function (req, res) {
+    var gelbeKarte = req.body;
+    console.log(gelbeKarte);
+    res.status(200).send('Eintrag erfolgreich hinzugefügt');
+});
+
+
+app.post('/inputGelbeKarten', urlencodedParser, function (req, res) {
+    console.log(req.body);
+    res.sendStatus(200);
+});
+
+
+app.post('/inputTor', function (req, res) {
+    var gelbeKarte = req.body;
+
+    console.log(gelbeKarte);
+
+    res.status(200).send('Eintrag erfolgreich hinzugefügt');
+
+});
+
+app.post('/inputVerschossen', function (req, res) {
+    var gelbeKarte = req.body;
+
+    console.log(gelbeKarte);
+
+    res.sendStatus(200).send('Eintrag erfolgreich hinzugefügt');
+
+});
+/* END SPORTSRADAR */
+
+
 io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
-    });
+    /* WETTER.COM */
+
+    /* ENDE WETTER.COM */
+
+
 });
