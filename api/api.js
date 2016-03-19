434f455800e4ed7ead3a612bca1bda2df9117d87
Markus Springer <markus.springer1994@googlemail.com>
2016-03-19 21:46:51 +0100

    Updated DB Adapter

--- api/api.js
+++ api/api.js
@@ -21,6 +21,43 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var connection = null;
 
+r.connect({ host: 'localhost', port: 28015}, function(err, conn) {
+    // Can't connect to Server
+    if (err) throw err;
+
+    connection = conn;
+
+    // Create Datebase for first init
+    r.dbCreate('tictactoe').run(connection, function(err) {});
+
+    // Map db to connection
+    connection.use('tictactoe');
+
+    r.tableList().run(connection, function (err, list) {
+        if (err) throw err;
+        checkTable(list, "user");
+        checkTable(list, "rooms");
+        checkTable(list, "turns");
+        checkTable(list, "wins");
+    });
+
+    // Check if table exists
+    function checkTable(list, tablename){
+        if (list.indexOf(tablename) == -1) {
+            r.tableCreate(tablename).run(conn, function(err, result) {
+                if (err) throw err;
+                console.log('created table ', tablename);
+            });
+        } else {
+            // For testing:
+            // Delete all Entries on startup
+            r.table(tablename).delete().run(conn, function(){
+                console.log('deleted all entries in ', tablename);
+            });
+        }
+    }
+
+});
 
 server.listen(3000);
 
